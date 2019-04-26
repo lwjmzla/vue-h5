ce1724ee7e3f715e112493474918dc8a1ed6c178
lwjmzla <214245088@qq.com>
2019-04-27 00:26:30 +0800

    111

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,13 +1,19 @@
 <template>
     <div class="hello">
        首页1
+       <span>1111</span>
     </div>
 </template>
 
 <script>
 </script>
 <style scoped lang="scss">
+/*有no的话  px不会转rem*/
 .hello{
-    font-size:32px;color:red;
+    font-size:32px;/*no*/
+    color:red;
+    span{
+        font-size: 32px;
+    }
 }
 </style>
